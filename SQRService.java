ab7f55161711adb4e3a99f8091af4d75677aae56
Pain <vadimklimkin1@gmail.com>
2023-02-25 07:07:51 +0300

    Initial commit

---
+++ SQRService.java
@@ -0,0 +1,18 @@
+package ru.netology.sqr;
+
+public class SQRService {
+
+    public int countSquares(int minimum, int maximum) {
+        int count = 0;
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= minimum && i * i <= maximum) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
+
+
